b5b2417164f6f8fe2b0668a1b941bb3a23cf0062
Tommy <tommykindle83@gmail.com>
2019-07-23 18:17:49 -0700

    Refactoring finished for stretch

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import MovieCard from './MovieCard';
 const Movie = (props) => {
   const [movie, setMovie] = useState();
 
@@ -32,26 +32,28 @@ const Movie = (props) => {
 
   const { title, director, metascore, stars } = movie;
   return (
-    <div className="save-wrapper">
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
 
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </div>
+    <div className="save-wrapper">
+      <MovieCard movie={movie} />
       <div className="save-button">Save</div>
     </div>
   );
 }
 
 export default Movie;
+      /*<div className="movie-card">
+<h2>{title}</h2>
+<div className="movie-director">
+Director: <em>{director}</em>
+</div>
+<div className="movie-metascore">
+Metascore: <strong>{metascore}</strong>
+</div>
+<h3>Actors</h3>
+
+{stars.map(star => (
+<div key={star} className="movie-star">
+{star}
+</div>
+))}
+</div>*/
